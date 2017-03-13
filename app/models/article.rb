8a7f15fcee9bfac1cb3b501df5401cad2af3f3a7
Felipe Vazquez Claro <Felipe@Felipes-iMac.local>
2017-03-13 10:09:19 -0300

    comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  has_many :comments
   validates :title, presence: true
   validates :subhead, presence: true,
                     length: { maximum: 250 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170311034131) do
+ActiveRecord::Schema.define(version: 20170311205240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,4 +23,14 @@ ActiveRecord::Schema.define(version: 20170311034131) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id", using: :btree
+  end
+
+  add_foreign_key "comments", "articles"
 end
